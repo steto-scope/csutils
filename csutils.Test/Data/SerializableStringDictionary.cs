cc96bf979c43b53681674bd5668c173ceef7dce7
recursivebytes <dev@recursivebytes.com>
2015-04-20 20:59:06 +0200

    SerializableStringDictionary

---
+++ csutils.Test/Data/SerializableStringDictionary.cs
@@ -0,0 +1,77 @@
+﻿using csutils.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace csutils.Test.Data
+{
+	[TestFixture]
+	public class SerializableStringDictionary
+	{
+
+		[TestCase]
+		public void TestXmlSerialisation()
+		{
+			SerializableStringDictionary<int> id = new SerializableStringDictionary<int>();
+			id["Test"] = 42;
+			id["Test2"] = 43;
+
+
+			try
+			{
+				XmlSerializer s = new XmlSerializer(typeof(SerializableStringDictionary<int>));
+				MemoryStream ms = new MemoryStream();
+				s.Serialize(ms, id);
+
+				ms.Seek(0, SeekOrigin.Begin);
+
+				XmlSerializer s2 = new XmlSerializer(typeof(SerializableStringDictionary<int>));
+				SerializableStringDictionary<int> d2e = (SerializableStringDictionary<int>)s2.Deserialize(ms);
+
+				Assert.IsTrue(d2e.ContainsKey("Test"));
+				Assert.AreEqual(d2e["Test"], id["Test"]);
+				Assert.AreEqual(d2e["Test2"], id["Test2"]);
+				
+			}
+			catch
+			{
+			}
+
+
+
+			SerializableStringDictionary<NonSerializableClass> id2 = new SerializableStringDictionary<NonSerializableClass>();
+			id2["Test"] = new NonSerializableClass() { Prop = "Foo" };
+
+
+			try
+			{
+				XmlSerializer s = new XmlSerializer(typeof(SerializableStringDictionary<NonSerializableClass>),new Type[]{typeof(NonSerializableClass)});
+				MemoryStream ms = new MemoryStream();
+				s.Serialize(ms, id2);
+
+				ms.Seek(0, SeekOrigin.Begin);
+
+				XmlSerializer s2 = new XmlSerializer(typeof(SerializableStringDictionary<NonSerializableClass>),new Type[]{typeof(NonSerializableClass)});
+				SerializableStringDictionary<NonSerializableClass> d2e = (SerializableStringDictionary<NonSerializableClass>)s2.Deserialize(ms);
+
+				Assert.IsTrue(d2e.ContainsKey("Test"));
+				Assert.AreEqual(d2e["Test"].Prop, id2["Test"].Prop);
+			}
+			catch
+			{
+
+			}
+
+		}
+
+	}
+	public class NonSerializableClass
+	{
+		public string Prop { get; set; }
+	}
+}
---
+++ csutils/Data/SerializableStringDictionary.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace csutils.Data
+{
+	/// <summary>
+	/// Simplified Version of SerializableDictionary for use with string keys
+	/// </summary>
+	/// <typeparam name="TKey">key type</typeparam>
+	/// <typeparam name="TValue">value type</typeparam>
+	public class SerializableStringDictionary<TValue> : Dictionary<string, TValue>, IXmlSerializable
+	{
+		/// <inheritdoc />
+		public System.Xml.Schema.XmlSchema GetSchema()
+		{
+			return null;
+		}
+
+		/// <inheritdoc />
+		public SerializableStringDictionary()
+			: base()
+		{
+
+		}
+		/// <inheritdoc />
+		public SerializableStringDictionary(int capacity)
+			: base(capacity)
+		{
+
+		}
+		/// <inheritdoc />
+		public SerializableStringDictionary(IDictionary<string, TValue> dictionary)
+			: base(dictionary)
+		{
+
+		}
+		/// <inheritdoc />
+		public virtual void ReadXml(XmlReader reader)
+		{
+			Boolean wasEmpty = reader.IsEmptyElement;
+			reader.Read();
+			if (wasEmpty)
+				return;
+
+			while (reader.NodeType != XmlNodeType.EndElement)
+			{
+				string key = reader.Name;
+				Type valuetype = (reader.HasAttributes) ? Type.GetType(reader.GetAttribute("type")) : null;
+				reader.Read();										
+				
+				if (valuetype != null)
+				{
+					//reader.Read();
+					Add(key, (TValue)new XmlSerializer(valuetype).Deserialize(reader));
+					//reader.ReadEndElement();
+				}
+				else
+				{
+					Add(key, default(TValue));
+				//	reader.Skip();
+				}
+				reader.ReadEndElement();
+				reader.MoveToContent();				
+			}
+			reader.ReadEndElement();
+		}
+		/// <inheritdoc />
+		public virtual void WriteXml(XmlWriter writer)
+		{
+			for (int i = 0; i < Keys.Count; i++)
+			{
+				string key = Keys.ElementAt(i);
+				TValue value = this.ElementAt(i).Value;
+
+				writer.WriteStartElement(key);
+				
+				if (value != null)
+				{
+					writer.WriteAttributeString(string.Empty, "type", string.Empty, value.GetType().AssemblyQualifiedName);
+					new XmlSerializer(value.GetType()).Serialize(writer, value);
+				}
+				writer.WriteEndElement();
+			}
+		}
+	}
+}
