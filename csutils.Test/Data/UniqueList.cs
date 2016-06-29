e7807e4910357491c0b16a23f43f67f647f217cf
recursivebytes <dev@recursivebytes.com>
2016-06-29 19:42:04 +0200

    Added: UniqueList<T>

---
+++ csutils.Test/Data/UniqueList.cs
@@ -0,0 +1,57 @@
+﻿using csutils.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csutils.Test.Data
+{
+    [TestFixture]
+    public class UniqueListTest
+    {
+        [TestCase]
+        public void TestAdd()
+        {
+            UniqueList<int> l = new UniqueList<int>();
+            l.Add(1);
+            Assert.AreEqual(1, l.Count);
+            l.Add(1);
+            Assert.AreEqual(1, l.Count);
+            l.Add(2);
+            Assert.AreEqual(2, l.Count);
+
+            l.AddRange(new int[] { 2, 3, 4 });
+            Assert.AreEqual(4, l.Count);
+            l.AddRange(new int[] { 1, 2, 3, 4 });
+            Assert.AreEqual(4, l.Count);
+
+            l.Insert(0, 42);
+            Assert.AreEqual(42, l[0]);
+            Assert.AreEqual(5, l.Count);
+
+            l.InsertRange(0, new int[] { 2, 99 });
+            Assert.AreEqual(99, l[0]);
+            Assert.AreEqual(6, l.Count);
+        }
+
+
+        [TestCase]
+        public void TestRemove()
+        {
+            UniqueList<int> l = new UniqueList<int>();
+            l.AddRange(new int[] { 1,2,3,4,5,6 });
+            l.Remove(6);
+            Assert.AreEqual(5, l.Count);
+
+            l.RemoveRange(0, 2);
+            Assert.AreEqual(3, l.Count);
+
+            l.RemoveAt(0);
+            Assert.AreEqual(2, l.Count);
+            Assert.AreEqual(4, l[0]);          
+            
+        }
+    }
+}
---
+++ csutils/Data/UniqueList.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace csutils.Data
+{
+    /// <summary>
+    /// Subclass of List&lt;T&gt;. Extends the List-functionality by preventing adding duplicates (similar to HashSet&lt;T&gt;). 
+    /// Implements INotifyPropertyChanged that fires when Items are added or removed.
+    /// NOTICE: Implementation is based on hiding original methods. Don't use Add/Insert/Remove on an object that is casted to List&lt;T&gt;
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class UniqueList<T> : List<T>, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Adds an item if not already in the List
+        /// </summary>
+        /// <param name="item"></param>
+        public new void Add(T item)
+        {
+            if (!this.Contains(item))
+            {
+                base.Add(item);
+                OnPropertyChanged("Count");
+            }
+        }
+
+        /// <summary>
+        /// Adds items, that aren't contained in the List
+        /// </summary>
+        /// <param name="items"></param>
+        public new void AddRange(IEnumerable<T> items)
+        {
+            var subset = this.Union(items).Except(this);
+            base.AddRange(subset);
+
+            if (subset.Count() > 0)
+                OnPropertyChanged("Count");
+        }
+
+        /// <summary>
+        /// Inserts an item if it isn't already part of the List
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        public new void Insert(int index, T item)
+        {
+            if (!this.Contains(item))
+            {
+                base.Insert(index, item);
+                OnPropertyChanged("Count");
+            }
+        }
+
+        /// <summary>
+        /// Inserts items that are not already contained in the List
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="items"></param>
+        public new void InsertRange(int index, IEnumerable<T> items)
+        {
+            var subset = this.Union(items).Except(this);
+            base.InsertRange(index, subset);
+
+            if (subset.Count() > 0)
+                OnPropertyChanged("Count");
+        }
+
+        /// <summary>
+        /// Removes an item
+        /// </summary>
+        /// <param name="item"></param>
+        public new void Remove(T item)
+        {
+            base.Remove(item);
+            OnPropertyChanged("Count");
+        }
+
+        /// <summary>
+        /// Removes a range of items
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="count"></param>
+        public new void RemoveRange(int index, int count)
+        {
+            base.RemoveRange(index, count);
+            OnPropertyChanged("Count");
+        }
+
+        /// <summary>
+        /// Removes the item at the given index
+        /// </summary>
+        /// <param name="index"></param>
+        public new void RemoveAt(int index)
+        {
+            base.RemoveAt(index);
+            OnPropertyChanged("Count");
+        }
+
+        /// <summary>
+        /// Removes the items that Match the Predicate
+        /// </summary>
+        /// <param name="match"></param>
+        public new void RemoveAll(Predicate<T> match)
+        {
+            base.RemoveAll(match);
+            OnPropertyChanged("Count");
+        }
+
+        /// <summary>
+        /// Clears the List
+        /// </summary>
+        public new void Clear()
+        {
+            base.Clear();
+            OnPropertyChanged("Count");
+        }
+
+        /// <summary>
+        /// PropertyChanged-Event. Fires whenever Count is changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
